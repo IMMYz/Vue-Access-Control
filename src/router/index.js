8a399b882b9a4d27be9f06983aad446cc8e76d04
叶子 <yz@gfire.cn>
2018-04-25 15:20:40 +0800

    引入改造地图

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,12 @@ let baseRoute = [{
   path: '/login',
   name: '登录',
   component: (resolve) => require(['../views/login.vue'], resolve)
-}, {
+},{
+  path:'/map',
+  name:'地图',
+  component:(resolve) => require(['../views/map-test.vue'],resolve)
+},
+  {
   path: '/401',
   name: '无权访问',
   component: (resolve) => require(['../views/common/401.vue'], resolve)
